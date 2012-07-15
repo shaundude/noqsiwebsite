52eeae071b5df3160a8606755e895438d7e8f043
Matthew Wampler-Doty <matt@w-d.org>
2012-07-15 11:07:37 -0600

    Trying to hack AJAX page loading

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-// Make sure jQuery and svg are loaded!
+// Make sure jQuery and svg-web are loaded!
 // TODO: maybe use this hack? http://neighborhood.org/core/sample/jquery/append-to-head.htm 
 
-// Hack to control style of included SVGs at Top Level - requires SVGWeb
-window.onsvgload = function() {
+// Hack to control style of included SVGs at top level - requires svg-web
+
+var updateSVGs = function(element, styleFile) {
 	var svgNS = "http://www.w3.org/2000/svg";
-	var svgs = document.getElementsByTagName("object");
-	console.log(svgs);	
-	for (var i in svgs) {
-		svgDoc = svgs[i].contentDocument;
-		var styleElement = svgDoc.createElementNS(svgNS, "style");
-		styleElement.textContent = "@import url('/styles/master.css')";
-		svgDoc.documentElement.appendChild(styleElement);
+	var objs = element.getElementsByTagName("object");
+	for (var i in objs) {
+		if(objs[i].type === "image/svg+xml") {
+			svgDoc = objs[i].contentDocument;
+			var styleElement = svgDoc.createElementNS(svgNS, "style");
+			styleElement.textContent = 
+				"@import url('" + styleFile + "')";
+			svgDoc.documentElement.appendChild(styleElement);
+		} ;
 	}
-//	$('object').each( function(idx) { 
-//		var svgDoc = $(this)[0].contentDocument; 
-//		var styleElement = svgDoc.createElementNS(svgNS, "style");
-//	});
-};
+}
+
+var updateContent = function (id) { 
+	$("#content").load(id + ".html", function () { 
+		updateSVGs(document,'/styles/master.css'); 
+	});
+}
+
+window.addEventListener('SVGLoad', function () { 
+	updateSVGs(document,'/styles/master.css'); 
+});
