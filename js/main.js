59f0455a69b479853003405de2bfd82405f1412b
Matthew Wampler-Doty <matt@w-d.org>
2012-07-15 21:57:43 -0600

    Simplifying Javascript

--- js/main.js
+++ js/main.js
@@ -3,26 +3,6 @@
 
 // Hack to control style of included SVGs at top level - requires svg-web
 
-var updateSVGs = function(element, styleFile) {
-	var svgNS = "http://www.w3.org/2000/svg";
-	var objs = element.getElementsByTagName("object");
-	for (var i in objs) {
-		if(objs[i].type === "image/svg+xml") {
-			svgDoc = objs[i].contentDocument;
-			var styleElement = svgDoc.createElementNS(svgNS, "style");
-			styleElement.textContent = 
-				"@import url('" + styleFile + "')";
-			svgDoc.documentElement.appendChild(styleElement);
-		} ;
-	}
-}
-
 var updateContent = function (id) { 
-	$("#content").load(id + ".html", function () { 
-		updateSVGs(document,'/styles/master.css'); 
-	});
+	$("#content").load(id + '.html');
 }
-
-window.addEventListener('SVGLoad', function () { 
-	updateSVGs(document,'/styles/master.css'); 
-});
