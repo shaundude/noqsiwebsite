a7744fc38fe9a8b6bf01e1d8df6aa431515e4c06
Matthew Wampler-Doty <matt@w-d.org>
2012-07-19 22:53:47 -0600

    Reindenting

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 // Make sure jQuery and bbq plugin loaded!
-
-var updateContent = function (url) { 
-	$('#content').hide().load(url).fadeIn();
-}
+var updateContent = function (url) {
+  $('#content').hide().load(url).fadeIn();
+};
 
 // Load and set up the navbar 
 // * Prepend '#' to all of the URLs
 // * Set 'click' handlers for BBQ history
 $(function () {
-   $.get('/navbar.html', function(data){ 
-        $('#navbar').html(data);
-        $('#navbar a').each(function (idx,obj) { 
-             var _href = $(obj).attr('href');
-             $(obj).attr('href',"#"+_href); 
-	     $(obj).live('click',function (e) {
-		  $.bbq.pushState({'': _href});
-		  $('#navbar a').removeClass('selected');
-		  $(obj).addClass('selected');
-		  return false;
-             });
-        });
+  $.get('/navbar.html', function (data) {
+    $('#navbar').html(data);
+    $('#navbar a').each(function (idx, obj) {
+      var _href = $(obj).attr('href');
+      $(obj).attr('href', "#" + _href);
+      $(obj).live('click', function (e) {
+        $.bbq.pushState({ '': _href });
+        $('#navbar a').removeClass('selected');
+        $(obj).addClass('selected');
+        return false;
+      });
+    });
   });
 });
 
@@ -28,10 +27,10 @@ var defaultURL = '/content/home.html';
 // Use BBQ history for navigation
 $(function () {
   // Bind hash changes
-  $(window).bind( 'hashchange', function(e) {
-	  var url = $.bbq.getState( '' ) || defaultURL;
-	  updateContent(url);
+  $(window).bind('hashchange', function (e) {
+    var url = $.bbq.getState('') || defaultURL;
+    updateContent(url);
   });
   // Trigger initial hashchange
-  $(window).trigger( 'hashchange' );
+  $(window).trigger('hashchange');
 });
