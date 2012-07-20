0760a39fad7327d658dc2e846fe3d88eb09a9e4a
Matthew Wampler-Doty <matt@w-d.org>
2012-07-19 19:37:03 -0600

    Using BBQ for backward/forward buttons, some CSS changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-// Make sure jQuery and svg-web are loaded!
-// TODO: maybe use this hack? http://neighborhood.org/core/sample/jquery/append-to-head.htm 
+// Make sure jQuery and bbq plugin loaded!
 
-var updateContent = function (id) { 
-	$('#content').load('/content/' + id + '.html');
+var updateContent = function (url) { 
+	$('#content').hide().load(url).fadeIn();
 }
 
-$(window).ready( function () {
-  $("#navbar").load('/navbar.html');
+// Load and set up the navbar 
+// * Prepend '#' to all of the URLs
+// * Set 'click' handlers for BBQ history
+$(function () {
+   $.get('/navbar.html', function(data){ 
+        $('#navbar').html(data);
+        $('#navbar a').each(function (idx,obj) { 
+             var _href = $(obj).attr('href');
+             $(obj).attr('href',"#"+_href); 
+	     $(obj).live('click',function (e) {
+		  $.bbq.pushState({'': _href});
+		  $('#navbar a').removeClass('selected');
+		  $(obj).addClass('selected');
+		  return false;
+             });
+        });
+  });
+});
+
+var defaultURL = '/content/home.html';
+
+// Use BBQ history for navigation
+$(function () {
+  // Bind hash changes
+  $(window).bind( 'hashchange', function(e) {
+	  var url = $.bbq.getState( '' ) || defaultURL;
+	  updateContent(url);
+  });
+  // Trigger initial hashchange
+  $(window).trigger( 'hashchange' );
 });
