2abb04f804437facc1ff77814132a56fdcbb01d1
Shaun Oyakawa <shaundude@hotmail.com>
2012-07-20 16:38:52 -0600

    changing port to something more predictable

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ http.createServer(function (request, response) {
         }
     });
      
-}).listen(8125);
+}).listen(8000);
  
-console.log('Server running at http://127.0.0.1:8125/');
+console.log('Server running at http://127.0.0.1:8000/');
